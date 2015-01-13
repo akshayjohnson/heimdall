29d7ebe3f12b54b42983e3597d38ca8018af6ac2
Ahmed Azaan <azaan@outlook.com>
2015-01-13 22:49:47 +0530

    base styling for story + droid sans font

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var less = require('gulp-less');
 var browserify = require('gulp-browserify');
 var concat = require('gulp-concat');
 var argv = require('yargs').argv;
@@ -8,14 +9,24 @@ var argv = require('yargs').argv;
  */
 gulp.task('watch', function() {
     gulp.watch('heimdall/static/js/**/*.js', ['js']);
+    gulp.watch('heimdall/static/css/**/*.less', ['css']);
 });
 
 /**
  * Compile and build the JS files
  */
 gulp.task('js', function() {
-    gulp.src('heimdall/static/js/app.js')
+    return gulp.src('heimdall/static/js/app.js')
         .pipe(browserify({debug: !argv.production}))
         .pipe(concat('app-build.js'))
         .pipe(gulp.dest('heimdall/static/build/js'));
+});
+
+/**
+ * Compile LESS files to css and prefix it
+ */
+gulp.task('css', function() {
+    return gulp.src('heimdall/static/css/app.less')
+        .pipe(less())
+        .pipe(gulp.dest('heimdall/static/build/css'));
 });
